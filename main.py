53d287e01c30297b8fdece43647a398737667c82
Zach Little <zach.little@clearpathgps.com>
2022-04-24 13:50:59 -0700

    gatecode

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ pin = LED(18)
 
 app = Flask(__name__)
 
-@app.route("/12345")
+@app.route("/SmMGs491s4rKd&X&X$aqT&2kH$1rTQ23zJjMka33O@gQzg$Ny55y^do8GrtFcXxnGU&u4EY93VFC5996MuuPc8lvXHoVl@IyIQl")
 def main():
    pin.on()
    sleep(2)
